03aaf061ed8b8556893c1183a0db7f62895332d7
ysk <ysk.univ.1007@gmail.com>
2024-11-08 20:42:21 +0900

    [lib/mogura/imap_handler.rb] fix Style/OptionalBooleanParameter

--- lib/mogura/imap_handler.rb
+++ lib/mogura/imap_handler.rb
@@ -5,7 +5,7 @@ require "net/imap"
 
 module Mogura
   class IMAPHandler
-    def initialize(host, port = 143, usessl = false, certs = nil, verify = true,
+    def initialize(host, port = 143, usessl: false, certs: nil, verify: true,
                    auth_info: nil)
       @imap = Net::IMAP.new(host, port, usessl, certs, verify)
       @imap.authenticate(auth_info[:auth_type], auth_info[:user], auth_info[:password]) if auth_info
