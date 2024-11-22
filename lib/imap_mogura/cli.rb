54aef67d690328f1ead397189a936cb3ff407e79
ysk <ysk.univ.1007@gmail.com>
2024-11-22 23:21:22 +0900

    [lib/imap_mogura/cli.rb] add a helper method

--- lib/imap_mogura/cli.rb
+++ lib/imap_mogura/cli.rb
@@ -254,6 +254,18 @@ module ImapMogura
       filter_mails(imap_handler, rules, mailbox, search_keys, retry_count + 1, dry_run: dry_run)
     end
 
+    def handle_mail_fetch_error_and_preprocess_retrying(error, retry_count)
+      warn "failed to fetch mail (id = #{error.message_id} on mailbox #{error.mailbox}): #{error.bad_response_error_message}"
+
+      # if retry_count is over the threshold, abort processing
+      raise Thor::Error, "retry count is over the threshold, stop processing" unless retry_count < 3
+
+      warn "wait a moment..."
+
+      # wait a moment...
+      sleep 10
+    end
+
     def filter_mail(imap_handler, rules, mailbox, message_id, dry_run: false)
       mail = imap_handler.fetch_header(mailbox, message_id)
 
